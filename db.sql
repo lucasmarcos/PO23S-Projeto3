26ba6120ce0620ec00a4d0112a3bf649142bb2d9
Lucas Marcos <lucasmarcos@live.com>
2019-12-04 01:19:04 -0300

    modelo

--- db.sql
+++ db.sql
@@ -1,20 +1,31 @@
 CREATE TABLE cliente (
     codigo SERIAL PRIMARY KEY,
-    nome VARCHAR
+    nome VARCHAR,
+    cpf VARCHAR,
+    endereco VARCHAR,
+    bairro VARCHAR,
+    cep VARCHAR,
+    cidade VARCHAR,
+    telefone VARCHAR,
+    uf VARCHAR
 );
 
 CREATE TABLE produto (
     codigo SERIAL PRIMARY KEY,
-    nome VARCHAR
+    nome VARCHAR,
+    valor INTEGER
 );
 
 CREATE TABLE nota (
     codigo SERIAL PRIMARY KEY,
-    cliente INTEGER NOT NULL REFERENCES cliente(codigo)
+    cliente INTEGER NOT NULL REFERENCES cliente(codigo),
+    total INTEGER
 );
 
 CREATE TABLE produto_comprado (
     nota INTEGER NOT NULL REFERENCES nota(codigo),
     produto INTEGER NOT NULL REFERENCES produto(codigo),
-    quantidade INTEGER
+    quantidade INTEGER,
+    total INTEGER,
+    data TIMESTAMP
 );
