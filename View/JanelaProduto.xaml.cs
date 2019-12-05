d3181fda2904f4a548dfc435bd8ab76468ea2883
Lucas Marcos <lucasmarcos@live.com>
2019-12-05 20:15:29 -0300

    prod

--- View/JanelaProduto.xaml.cs
+++ View/JanelaProduto.xaml.cs
@@ -1,18 +1,32 @@
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
+using Projeto3.DAO;
+using Projeto3.Model;
+
 namespace Projeto3
 {
 	public class JanelaProduto : Window
 	{
-		public JanelaProduto()
+		private DAOProduto daoProduto;
+
+		public JanelaProduto(DAOProduto d)
 		{
+			daoProduto = d;
+			DataContext = new Produto();
 			InitializeComponent();
 		}
 
 		private void InitializeComponent()
 		{
 			AvaloniaXamlLoader.Load(this);
+		}
+
+		private void botaoCadastrar(object s, object e)
+		{
+			var produto = ((Produto) DataContext);
+			daoProduto.cadastrar(produto);
+			this.Close();
 		}
 	}
 }
