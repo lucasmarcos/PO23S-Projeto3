4a86ef1c312184e63e5995f2994b469fa1010a42
Lucas Marcos <lucasmarcos@live.com>
2019-12-11 02:05:45 -0300

    just trash

---
+++ MainForm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PO23S_Projeto3
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+	}
+}
